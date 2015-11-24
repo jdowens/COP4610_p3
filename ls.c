d14d4ad74b3a332677cf0cec84586009e5545220
John - Gregulak <gregulak@linprog1.cs.fsu.edu>
2015-11-24 13:43:56 -0500

    Added comments

--- ls.c
+++ ls.c
@@ -5,10 +5,13 @@ void list(){
 	//set up array to store names of directories and files to print
 	char * ls_print = malloc(10 * sizeof(char));
 
-	
-	
+	//check if this is a valid directory name
+	//check bytes 26-27 for the starting cluster
+	//locate the cluster number
 
-
+	/*
+	int bytes2627 = 
+	*/
 
 
 
