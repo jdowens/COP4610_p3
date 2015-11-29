f9d06f342aead569b4780c8937eb1442fe3633c0
jdowens <jdowensmusic@gmail.com>
2015-11-28 19:41:37 -0500

    Created cd functionality

---
+++ cd.c
@@ -0,0 +1,20 @@
+#include "cd.h"
+
+void cd(const char* path)
+{
+	struct DirectoryEntry* tmp = GetDirectoryContents(GetCurrentDirectoryClusterNum());
+	unsigned int index = 0;
+	unsigned char somethingFound = 0;
+	while (!tmp[index].END_OF_ARRAY)
+	{
+		if (strcmp(tmp[index].DIR_Name, path) == 0 && tmp[index].DIR_Attr & 0x10)
+		{
+			SetCurrentDirectoryClusterNum(tmp[index].DIR_FstClus);
+			somethingFound = 1;
+		}
+	}
+	if (somethingFound == 0)
+	{
+		printf("Not a valid directory.\n");
+	}
+}
---
+++ cd.h
@@ -0,0 +1,10 @@
+#ifndef _CD_H
+#define _CD_H
+
+#include <string.h>
+#include "directoryparse.h"
+#include "utility.h"
+
+void cd(const char* path);
+
+#endif
