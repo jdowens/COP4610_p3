f8a64b3ded269bb8ea68d4399fec0437f1b0ffc8
Aasin Jamil <jamil@linprog4.cs.fsu.edu>
2015-11-29 02:09:23 -0500

    cleaned up a little bit

--- parseinput.c
+++ parseinput.c
@@ -1,6 +1,4 @@
 #include "parseinput.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 const char PREVDIR[11] = "..         ";
 const char CURRDIR[11] = ".          ";
@@ -8,7 +6,8 @@ const char CURRDIR[11] = ".          ";
 void ToFAT32(char * input)
 {
    char FAT32string [13], temp [13];
-   short count_spaces = 11 - strlen(input);
+   short inputSize = strlen(input);
+   short count_spaces = 11 - inputSize;
 
    if(!strcmp(input, ".."))
    {
@@ -22,13 +21,13 @@ void ToFAT32(char * input)
        return;
    }
 
-   for(int i = 0; i < strlen(input); ++i)
+   for(int i = 0; i < inputSize; ++i)
    {
        if(input[i] >= 'a' && input[i] <= 'z')
            FAT32string[i] = input[i] - 32;
        else 
        {
-           if (input[i] == '.' && strlen(input) < 12)
+           if (input[i] == '.' && inputSize < 12)
            {
                count_spaces = 11-(i+3);
            }
@@ -36,12 +35,12 @@ void ToFAT32(char * input)
            FAT32string[i] = input[i];
        }
    }
-   for(int i = strlen(input); i < 12; ++i)
+   for(int i = inputSize; i < 12; ++i)
        FAT32string[i] = ' ';
 
    strcpy(temp, FAT32string);
 
-   for(int i = 0; i < strlen(FAT32string); ++i)
+   for(int i = 0; i < 11; ++i)
    {
        if(FAT32string[i] == '.')
        {
@@ -76,28 +75,31 @@ void ToStandardFormat(char * input)
 
 
    char exten[4];
-   char temp[12];
+   char temp[12] = " ";
 
    exten[0] = input[8];
    exten[1] = input[9];
    exten[2] = input[10];
    exten[3] = '\0';
 
-   for(int i = 0; i < strlen(input); ++i)
+   for(int i = 0; i < 4; ++i)
+       if(exten[i] == ' ')
+           exten[i] = '\0';
+
+   for(int i = 0; i < 11; i++)
    {
        if(input[i] == ' ')
        {
           if(exten[0] >= 'A' && exten[0] <= 'Z')
-          {
              temp[i] = '.';
-             strcat(temp, exten);
-          }
-          temp[i] = '\0';
+
+          strcat(temp, exten);
+          temp[i+strlen(exten)+1] = '\0';
           strcpy(input, temp);
           return;
        }
 
-       temp[i] = input[i];
+       temp[i] = input[i]; 
    }
    temp[11] = '\0';
    strcpy(input,temp);
