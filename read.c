b264fccde296e3bb33dbfbed5bfb426df0f8165f
Nicolas Marshall Cardenas <ncardena@linprog3.cs.fsu.edu>
2015-11-29 23:08:35 -0500

    added to read.c

--- read.c
+++ read.c
@@ -20,32 +20,30 @@ void read(const char* FILE_NAME, int POSITION, int NUM_BYTES,unsigned int curren
 	                        		POSITION = POSITION - 512;
                 			}//end while
 				}//end if
-
 				//read multi sectors
 				if(POSITION+NUM_BYTES >= 512){
+					int bytes_read = 512-POSITION;
 					//go to position in sector
                        			fseek(ImageFile,FindFirstSectorOfCluster(cluster_number)+POSITION,SEEK_SET);
 					//read to the end of sector then read from beginning of next sector
-					if( (POSITION+NUM_BYTES) > 512){//<--i think the while loop checks this already
-						fread(temp,sizeof(unsigned char),512-POSITION,ImageFile);
+					fread(temp,sizeof(unsigned char),bytes_read,ImageFile);
+					cluster_number = next_cluster(cluster_number);
+					NUM_BYTES = NUM_BYTES - bytes_read;
+					while(cluster_number < 0x0ffffff8 || NUM_BYTES < 512){
+						POSITION = 0;
+						//go to next position in sector
+                                               	fseek(ImageFile,FindFirstSectorOfCluster(cluster_number)+POSITION,SEEK_SET);
+						//read entire sector
+                                            	fread(temp,sizeof(unsigned char),512,ImageFile);
 						cluster_number = next_cluster(cluster_number);
-						while(cluster_number < 0x0ffffff8 || NUM_BYTES < 512){
-							POSITION = 0;
-							//go to next position in sector
-                                                	fseek(ImageFile,FindFirstSectorOfCluster(cluster_number)+POSITION,SEEK_SET);
-							//read entire sector
-                                               		fread(temp,sizeof(unsigned char),512,ImageFile);
-							cluster_number = next_cluster(cluster_number);
-							NUM_BYTES -= 512;
-						}
+						NUM_BYTES -= 512;
+					}//end while
 						POSITION = 0;
                                                 //go to next position in sector
                                                 fseek(ImageFile,FindFirstSectorOfCluster(cluster_number)+POSITION,SEEK_SET);
                                                 //read entire sector
-                                                fread(temp,sizeof(unsigned char),512,ImageFile);
-					}//end if
+                                                fread(temp,sizeof(unsigned char),NUM_BYTES,ImageFile);
 				}//end if
-
 				//read one sector only
 				else{
 					//read from only one sector
@@ -63,22 +61,22 @@ void read(const char* FILE_NAME, int POSITION, int NUM_BYTES,unsigned int curren
 
 void TestRead(){
 
-        const char* TempFileName = NULL;
-//	struct DirectoryEntry* directoryArray = GetDirectoryContents(GetCurrentDirectoryClusterNum());
-//	cd("BLUE       ");
-//	cd("BLUE2      ");
-//      int current_cluster = NameToClusterNumber("XL_DATA    ");
-        
-	unsigned int current_cluster = 0x26;
-	unsigned int current_cluster_address = 0x104c00;
+      const char* TempFileName = NULL;
+	struct DirectoryEntry* directoryArray = GetDirectoryContents(GetCurrentDirectoryClusterNum());
+	cd("BLUE       ");
+	cd("BLUE2      ");
+	int current_cluster = NameToClusterNumber("XL_DATA    ");
+//	unsigned int current_cluster = 0x26;
+//	unsigned int current_cluster_address = 0x104c00;
+
 	printf("\nTest Read Data Below: \n");
         printf("Cluster Number for XL_DATA: %x\n",current_cluster);
-        printf("Cluster Address: %x\n",current_cluster_address);
-  	printf("Next Cluster Number after 0x26: %x",next_cluster(0x26));    
+        printf("Cluster Address: %x\n",FindFirstSectorOfCluster(current_cluster));
+  	printf("Next Cluster Number after 0x26: %x",next_cluster(current_cluster));    
 
 	//figure out how to read in the dir size to display
 	//printf("File A size: %d\n",directoryArray[0].DIR_FileSize);
-//	read(TempFileName,0,7,current_cluster);
+	read(TempFileName,0,7,current_cluster);
 
 }
 
