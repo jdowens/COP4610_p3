98a4034d00105cfab8c6212b52aa0689f476acd0
Nicolas Marshall Cardenas <ncardena@linprog3.cs.fsu.edu>
2015-12-01 16:45:59 -0500

    changes to read

--- read.c
+++ read.c
@@ -1,36 +1,36 @@
 #include "read.h" 
 
 void read(const char* FILE_NAME, int POSITION, int NUM_BYTES){
-	        int cluster_number = NameToClusterNumber(FILE_NAME);
 		FILE* ImageFile = GetImageFile();
+	        int cluster_number = NameToClusterNumber(FILE_NAME);
 		unsigned char temp[512];
 		struct DirectoryEntry* directoryArray = GetDirectoryContents(GetCurrentDirectoryClusterNum());
-		unsigned int last_byte_loc =  NUM_BYTES-POSITION;
-		int print_bytes= NUM_BYTES;
 
-/*        	printf("\n\nTest Read Data Below: \n");
-        	printf("Cluster Number for XL_DATA: %x\n",cluster_number);
-        	printf("Cluster Address: %x\n",FindFirstSectorOfCluster(cluster_number));
-*/
-		//get dir names and sizes
-	        int index = 0;
-        	for(index = 0; !directoryArray[index].END_OF_ARRAY; index++){
-                	//figure out how to read in the dir size to display
-                	if( strcmp( FILE_NAME,directoryArray[index].DIR_Name ) == 0 ){
-                        	printf("File Name: %s\n",directoryArray[index].DIR_Name);
-                        	printf("File size: %d\n",directoryArray[index].DIR_FileSize);
-                        	break;
-                	}
-        	}
+                //get dir names and sizes
+                int index = 0;
+                for(index = 0; !directoryArray[index].END_OF_ARRAY; index++){
+                        if( strcmp( FILE_NAME,directoryArray[index].DIR_Name ) == 0 ){
+                                printf("File Name: %s\n",directoryArray[index].DIR_Name);
+                                printf("File size: %d\n\n",directoryArray[index].DIR_FileSize);
+                                break;
+                        }
+                }
+
         	if(directoryArray[index].END_OF_ARRAY){
                		printf("Did not find the name.\n");
 			return;
         	}
-
+/*
+		if(FTIsOpenInRead(FILE_NAME)==0){
+			printf("Cannot access this file.\n");
+			return;
+		}
+*/
 		if( (POSITION > directoryArray[index].DIR_FileSize) || (NUM_BYTES > directoryArray[index].DIR_FileSize)){
 			printf("Error: Out of File Size.\n");
+			return;
 		}
-			
+
 		else{
 				if(POSITION >= 512){
 					//while statement will  go the the cluster with the starting position
@@ -86,11 +86,46 @@ void read(const char* FILE_NAME, int POSITION, int NUM_BYTES){
 		}//end of else
 }
 
+
+/* Compare file name then CD and recompare until file found. */
+void valid_file(const char* FILE_NAME){	
+	struct DirectoryEntry* directoryArray = GetDirectoryContents(GetCurrentDirectoryClusterNum());
+        int root_index = 0;
+	int cd_index = 0;
+	int dir_index = 0;
+
+	for(root_index = 0; !directoryArray[root_index].END_OF_ARRAY; root_index++){
+		printf("%s %x",directoryArray[root_index].DIR_Name,directoryArray[dir_index].DIR_Attr);
+		if( strcmp( FILE_NAME,directoryArray[root_index].DIR_Name ) == 0 ){
+                	printf("File Found =D");
+                	return;
+                }//end if
+		else{
+			//I think this checks if name is a directory
+			while(directoryArray[dir_index].DIR_Attr == 0x10 && !directoryArray[root_index].END_OF_ARRAY){
+				printf("%s ",directoryArray[root_index].DIR_Name);
+				cd(directoryArray[dir_index].DIR_Name);
+				for(cd_index = 0; !directoryArray[cd_index].END_OF_ARRAY; cd_index++){
+					if( strcmp( FILE_NAME,directoryArray[cd_index].DIR_Name ) == 0 ){
+		                        	printf("File Found =D");
+                		               	return;
+					}//end if
+				}//end for
+				dir_index++;
+			}//end while
+		}//end else
+	}//end for
+	printf("\nError: File not found.\n");
+}
+
+
+
 void TestRead(){
 	int NUM_BYTES = 7;
 	int POSITION = 0;
 	const char* TempFileName = "XL_DATA    ";
 	cd("BLUE       ");
 	cd("BLUE2      ");
+//	valid_file(TempFileName);
 	read(TempFileName,POSITION,NUM_BYTES);
 }
--- read.h
+++ read.h
@@ -1,10 +1,16 @@
 #ifndef _READ_H
 #define _READ_H
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "utility.h"
 #include "directoryparse.h"
+#include "filetable.h"
+#include "cd.h"
 
 void read(const char*,int,int);
+void valid_file(const char*);
 void TestRead();
+
 #endif
