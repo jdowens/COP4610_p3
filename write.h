c7e7f8de8550c5d11a08afbb54bed4bd2ddc20d2
Nicolas Marshall Cardenas <ncardena@linprog3.cs.fsu.edu>
2015-12-02 15:53:01 -0500

    added to write

--- write.h
+++ write.h
@@ -1,10 +1,12 @@
 #ifndef _WRITE_H
 #define _WRITE_H
 #include <string.h>
+#include <stdio.h>
 #include "cd.h"
 #include "filetable.h"
 #include "size.h"
 #include "utility.h"
+#include "parseinput.h"
 
 void write(const char*, int, int, const char*);
 void TestWrite();
