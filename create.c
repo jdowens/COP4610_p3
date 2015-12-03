1778ff028fb084b6b162050958ca3b2d5b0b72c7
jdowens <jdowensmusic@gmail.com>
2015-12-02 19:55:52 -0500

    Create functionality working

---
+++ create.c
@@ -0,0 +1,25 @@
+#include "create.h"
+
+void create(const char* name)
+{
+	struct DirectoryEntry* tmp = GetDirectoryContents(GetCurrentDirectoryClusterNum());
+
+	int i;
+	for (i = 0; !tmp[i].END_OF_ARRAY; i++)
+	{
+		if (strcmp(tmp[i].DIR_Name, name) == 0)
+		{
+			printf("File already exists!\n");
+			return;
+		}
+	}
+
+	struct DirectoryEntry ent;
+	strcpy(ent.DIR_Name, name);
+	ent.DIR_Attr = 0x00;
+	unsigned int firstCluster = FindNextFreeCluster();
+	WriteToFAT(firstCluster, 0x0FFFFFF8);
+	ent.DIR_FstClus = firstCluster;
+	ent.DIR_FileSize = 0x0000;
+	WriteDirectoryEntry(ent, GetCurrentDirectoryClusterNum());
+}
